6eebf0944d08b1dbf28a5b307e9534586aedbed4
TharunBalaji2004 <tharunbalaji31@gmail.com>
2023-08-24 21:15:32 +0530

    Implemented Food Donate form

--- server.py
+++ server.py
@@ -2,6 +2,10 @@ from flask import Flask ,redirect, render_template, request, url_for
 
 app = Flask(__name__, static_url_path='/static')
 
+charity = ["Safe Chennai Orphanage,Adayar", "Child safe Orphanage,Avadi", "Elder Orphanage,Villivakkam", "Amirtha Orphanage,Redhills", "Nanda Orphanage,Ambattur", "Cauvery charity trust,Adayar", "Hari trust,Korattur", "Radha Elder Orphanage,OMR"]
+quantity = ["Less than 20 people","25-50 people","50-100 people","100-200 people","200-300 people","300-400 people","400-500 people","500 above"]
+transport = ["Delivery Van", "Car", "Bulk Carriers", "Cargo Vans", "Box Trucks", "Motorcycles", "Tanker Trucks"]
+
 @app.route('/') # Homepage - index.html
 def home():
     return render_template('index.html')
@@ -21,6 +25,24 @@ def login():
 @app.route('/donate')
 def donate():
     return render_template('fooddonate.html')
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit_form():
+    if request.method == 'POST':
+        form_data = request.form
+
+        orgName = form_data['orgname']
+        personName = form_data['personname']
+        contactNumber = form_data['contact']
+        email = form_data['email']
+        foodQuantity = quantity[int(form_data['food-quantity'])]
+        charityName = charity[int(form_data['charity-name'])]
+        transportRequired = transport[int(form_data['transport-required'])]
+        ngoMember = form_data['ngomember']
+
+        print(f'{orgName} {personName} {contactNumber} {email} {foodQuantity} {charityName} {transportRequired} {ngoMember}')
+        
+    return redirect(url_for('donate'))
 
 if __name__ == "__main__":
     app.run()
