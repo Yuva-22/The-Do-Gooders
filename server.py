9aa5aab8656ac2f39904040bd2f1aa61c3ff2b3b
TharunBalaji2004 <tharunbalaji31@gmail.com>
2023-08-24 14:53:26 +0530

    Initial Project setup

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask ,redirect, render_template, request, url_for
+
+app = Flask(__name__)
+
+@app.route('/') # Homepage - index.html
+def home():
+    return render_template('index.html')
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html')
+
+@app.route('/contactus')
+def contactus():
+    return render_template('contactus.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/donate')
+def donate():
+    return render_template('fooddonate.html')
+
+if __name__ == "__main__":
+    app.run()
